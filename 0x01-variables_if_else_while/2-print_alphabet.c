e2b47e2f08cf0ac9078c2cf72d3829966cd4e555
yunus165 <alhassanyunus339@gmail.com>
2023-06-26 04:11:40 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  * Description - 'putchar function'
+  * Return: always zero (success)
+*/
+int main(void)
+{
+	int n = 97;
+	
+	while (n <= 122)
+	{
+		putchar('n');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
